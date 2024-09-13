bba1951658d7db1dfdc3d7f9651728dcb9507474
Mathieu Bellon <mathieu.bellon@gitguardian.com>
2024-09-13 13:10:49 +0200

    test token

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const asana_id := "1207970347157002"
+const asana_secret := "65522b9d841961357beff9f0647879de"
+
+const gh := "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" 
+
 func main() {
 	fmt.Println("Hello, World!")
 }
