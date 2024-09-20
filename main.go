bb8296f4dc47ad628a33c6e5beca4ecd06b9353a
Mathieu Bellon <mathieu.bellon@gitguardian.com>
2024-09-20 09:11:37 +0200

    still error

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 const asana_id = "1207970347157002"
 const asana_secret = "65522b9d841961357beff9f0647879de"
 
-var newgh2 string = "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"
+var newgh3 string = "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"
 
 func main() {
 	fmt.Println("Hello, World!")
