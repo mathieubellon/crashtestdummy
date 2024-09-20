38e09ffdc4620edc8acc031c65de42c4fd43dac3
Mathieu Bellon <mathieu.bellon@gitguardian.com>
2024-09-20 17:57:35 +0200

    still mega live stripe2

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const asana_id = "1207970347157002"
+const asana_secret = "65522b9d841961357beff9f0647879de"
+
+var newgh3 string = "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"
+
+var connector = "postgres://mathieu:8FH7u3JavBX0OPxNhHJGh@127.0.0.1:5432/crashtest"
+
 func main() {
 	fmt.Println("Hello, World!")
 }
